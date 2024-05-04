d6194732251bca05ffcf75e62e09a09fd3e6eaa4
Mungai <mungaij697@gmail.com>
2024-05-04 13:39:22 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
+
+// import username from "./user.js";
+// import city from "./user.js";
+// import image from "./user.js";
+
 
 function App() {
   return (
@@ -6,6 +14,9 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      {/* <username />
+      <city />
+      <image /> */}
     </div>
   );
 }
